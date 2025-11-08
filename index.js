00d973d6a5b94ef32a4fcc8cbd3bd5cb4731805c
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-11-07 22:25:48 -0300

    aprendendo sobre como testar a api com thunder client

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("olá")
+const express = require("express")
+const app = express()
+app.get('/', function (req, res) {
+    res.send('Hello Word')
+})
+
+app.get('/oi', function (res, req) {
+    res.send('Olá mundo!')
+})
+
+app.listen(3000)
